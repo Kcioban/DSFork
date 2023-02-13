44b78a942408181d5a1752479c01b49a1c99d767
Reenye Lima <reenyelima@hotmail.com>
2023-02-13 11:55:54 -0300

    projetos aula04

--- 3des/01-proj/aula04/agenda/front/index.js
+++ 3des/01-proj/aula04/agenda/front/index.js
@@ -1,8 +1,9 @@
-const url = 'http://localhost:3000'
+const uri = 'http://localhost:3000'
 const cards = document.querySelector('#cards')
+const create = document.querySelector('#create')
 
 const load = () => {
-    fetch(url + '/read', { method: 'GET' })
+    fetch(uri + '/read', { method: 'GET' })
         .then(response => response.json())
         .then(response => compromissos(response.Compromissos))
         .catch(err => console.error(err));
@@ -12,28 +13,92 @@ const compromissos = (vetor) => {
     vetor.forEach(e => {
         const card = document.createElement('form')
         const quando = document.createElement('input')
-        const hora = document.createElement('input')
         const onde = document.createElement('input')
         const descricao = document.createElement('textarea')
         const up = document.createElement('button')
         const del = document.createElement('button')
-        quando.type = 'date'
-        quando.value = e.quando.split('T')[0]
-        hora.type = 'time'
-        hora.value = e.quando.split('T')[1].substring(0, 5)
+        quando.type = 'datetime-local'
+        quando.value = e.quando.substring(0, 16)
         onde.value = e.onde
         descricao.innerHTML = e.descricao
         up.type = 'submit'
         up.innerHTML = "*"
-        del.id = e._id
+        del.type = 'button'
+        del.addEventListener('click', (ev) => {
+            if (confirm('Confirma a exclusão do compromisso: ' + e._id + '?')) {
+                fetch(uri + '/delete/' + e._id, { method: 'DELETE' })
+                    .then(resp => resp.status)
+                    .then(resp => {
+                        if (resp == 204)
+                            window.location.reload();
+                    })
+                    .catch(err => console.error(err));
+            }
+        })
         del.innerHTML = "-"
         card.appendChild(quando)
-        card.appendChild(hora)
         card.appendChild(onde)
         card.appendChild(descricao)
         card.appendChild(up)
         card.appendChild(del)
+        card.addEventListener('submit', (ev) => {
+            ev.preventDefault()
+            const compromisso = {
+                quando: new Date(quando.value),
+                onde: onde.value || 'Branco',
+                descricao: descricao.value || 'Branco'
+            }
+            const options = {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(compromisso)
+            };
+            fetch(uri + '/update/' + e._id, options)
+                .then(resp => resp.status)
+                .then(resp => {
+                    if (resp == 202)
+                        window.location.reload();
+                    else
+                        alert("Erro ao enviar dados para o servidor, erro: " + resp)
+                })
+                .catch(err => console.error(err));
+        })
         cards.appendChild(card)
     })
 }
 
+create.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const compromisso = {
+        quando: new Date(create.quando.value),
+        onde: create.onde.value || 'Branco',
+        descricao: create.descricao.value || 'Branco'
+    }
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(compromisso)
+    };
+    fetch(uri + '/create', options)
+        .then(resp => resp.status)
+        .then(resp => {
+            if (resp == 201)
+                window.location.reload();
+            else
+                alert("Erro ao enviar dados para o servidor, erro: " + resp)
+        })
+        .catch(err => console.error(err));
+});
+
+const del = (id) => {
+    if (confirm('Confirma a exclusão do compromisso: ' + id + '?')) {
+        const options = { method: 'DELETE' };
+        fetch(uri + '/delete/' + id, options)
+            .then(resp => resp.status)
+            .then(resp => {
+                if (resp == 204)
+                    window.location.reload();
+            })
+            .catch(err => console.error(err));
+    }
+}
