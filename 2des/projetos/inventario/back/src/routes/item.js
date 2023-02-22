a519e748d690a8ae8e3b02ca1c5b5b2816ac72b9
wellifabio <wellifabio@hotmail.com>
2023-02-22 11:11:50 -0300

    pwbe aula06

--- 2des/projetos/inventario/back/src/routes/item.js
+++ 2des/projetos/inventario/back/src/routes/item.js
@@ -6,6 +6,6 @@ const Item = require('../controllers/item');
 router.get('/', Item.teste);
 router.get('/item/listar', Item.listar);
 router.post('/item/criar', Item.criar);
-router.delete('/item/excluir', Item.excluir);
+router.delete('/item/excluir/:id', Item.excluir);
 
 module.exports = router
