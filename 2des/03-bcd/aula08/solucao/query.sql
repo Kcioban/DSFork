9ba3a6a8918dcfb0c065869cc00a5ad67d342000
wellifabio <wellifabio@hotmail.com>
2023-03-10 16:58:37 -0300

    op_mic excel aula02

--- 2des/03-bcd/aula08/solucao/query.sql
+++ 2des/03-bcd/aula08/solucao/query.sql
@@ -18,10 +18,13 @@ UPDATE estacionamento SET
 -- 4. Crie uma query que mostre todos os dados da view vw_estacionamento em ordem de saída decrescente
 -- também acrescente as colunas nome do cliente, modelo e cor do veículo e tipo da vaga
 -- Salve esta consulta como vw_estacionamento_full
+CREATE VIEW vw_estacionamento_full AS
 SELECT v.id, v.placa, ve.modelo, ve.cor, v.cpf, c.nome, v.id_vaga, va.tipo, v.entrada, v.saida, v.valor, v.tempo_total
 FROM vw_estacionamento v
 INNER JOIN cliente c ON v.cpf = c.cpf
 INNER JOIN veiculo ve ON v.placa = ve.placa
 INNER JOIN vaga va ON v.id_vaga = va.id
-ORDER BY v.saida DESC
-limit 10;
+ORDER BY v.saida DESC;
+
+SELECT * FROM vw_estacionamento;
+SELECT * FROM vw_estacionamento_full;
