6c4c3c7a89d11287cfff94fd07a1be4425a25fe0
wellifabio <wellifabio@hotmail.com>
2023-03-29 11:23:10 -0300

    indmo

--- 2des/04-indmo/aula02/App.js
+++ 2des/04-indmo/aula02/App.js
@@ -1,11 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Switch, Image } from 'react-native';
+
+const icon = require('./assets/icon.png');
 
 export default function App() {
+  const [habilitado, setHabilitado] = new useState(false);
+
+  const habilitar = () => {
+    setHabilitado(!habilitado);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+        source={icon}
+        style={styles.icon}
+      />
+      <Switch 
+        value={habilitado}
+        onValueChange={habilitar}
+      />
+
+      <Image 
+        source={{
+          uri : (habilitado) 
+                ? "https://cdn-icons-png.flaticon.com/512/702/702797.png" 
+                : "https://cdn-icons-png.flaticon.com/512/702/702814.png"
+        }}
+        style={styles.lampada}
+      />
+
+      {/* {
+        (habilitado) 
+        ?
+        <Image 
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/512/702/702797.png"
+          }}
+          style={styles.lampada}
+        />
+        :
+        <Image 
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/512/702/702814.png"
+          }}
+          style={styles.lampada}
+        />
+      } */}
+
     </View>
   );
 }
@@ -16,5 +58,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: '32px'
   },
+  icon: {
+    width: '68px',
+    height: '68px'
+  },
+  lampada: {
+    width: '125px',
+    height: '125px'
+  }
 });
