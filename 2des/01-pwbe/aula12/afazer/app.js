654e3a0cb62508bb50b3e140a91996704f2101ce
wellifabio <wellifabio@hotmail.com>
2023-04-04 10:17:39 -0300

    aula12

--- 2des/01-pwbe/aula12/afazer/app.js
+++ 2des/01-pwbe/aula12/afazer/app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const cors = require('cors')
 const methodOverride = require('method-override')
 
-//const router = require('./src/routes/tarefas')
+const router = require('./src/routes/tarefa')
 
 const app = express()
 app.use(express.json())
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
-//app.use('/', router)
+app.use('/', router)
 app.use(express.static('public'))
 
 app.listen(PORT, () => {
