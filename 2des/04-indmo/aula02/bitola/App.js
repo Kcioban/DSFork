c8538bfe4025c19b806141c5681ae7c3e0764d69
wellifabio <wellifabio@hotmail.com>
2023-04-26 09:54:37 -0300

    aula02

---
+++ 2des/04-indmo/aula02/bitola/App.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  const [distancia, setDistancia] = new useState(0)
+  const [corrente, setCorrente] = new useState(0)
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Cálculo de Bitola</Text>
+      <Text style={styles.textos}>Distância em metros:</Text>
+      <TextInput
+        style={styles.inputs}
+        value={distancia}
+        onChangeText={(val) => { setDistancia(Number(val))}}
+      />
+      <Text style={styles.textos}>Valor da corrente:</Text>
+      <TextInput
+        style={styles.inputs}
+        value={corrente}
+        onChangeText={(val) => { setCorrente(Number(val))}}
+      />
+      <Button title='Calcular'/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#330000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: '22px',
+    color: '#fcc',
+    fontWeight: 'bold',
+  },
+  textos: {
+    fontSize: '18px',
+    color: '#fee',
+  },
+  inputs: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: '1px',
+    width: '100%',
+    padding: '10px',
+    color: '#fcc',
+    fontSize: '16px',
+  },
+  botao:{
+
+  }
+});
