045105d33b78fc4c87e505ae102f0da1a26da0f4
wellifabio <wellifabio@hotmail.com>
2023-04-26 10:04:34 -0300

    aula02

--- 2des/04-indmo/aula02/bitola/App.js
+++ 2des/04-indmo/aula02/bitola/App.js
@@ -5,6 +5,9 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function App() {
   const [distancia, setDistancia] = new useState(0)
   const [corrente, setCorrente] = new useState(0)
+  const calcular = () => {
+    console.log('Pressionou')
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>Cálculo de Bitola</Text>
@@ -12,15 +15,19 @@ export default function App() {
       <TextInput
         style={styles.inputs}
         value={distancia}
-        onChangeText={(val) => { setDistancia(Number(val))}}
+        onChangeText={(val) => { setDistancia(Number(val)) }}
       />
       <Text style={styles.textos}>Valor da corrente:</Text>
       <TextInput
         style={styles.inputs}
         value={corrente}
-        onChangeText={(val) => { setCorrente(Number(val))}}
+        onChangeText={(val) => { setCorrente(Number(val)) }}
       />
-      <Button title='Calcular'/>
+      <Button
+        style={styles.botao} title='Calcular'
+        color='#000033'
+        onPress={calcular}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -49,8 +56,5 @@ const styles = StyleSheet.create({
     padding: '10px',
     color: '#fcc',
     fontSize: '16px',
-  },
-  botao:{
-
   }
 });
