30823e96ccab11fad5cf3dee92cb52e80c7bbf1f
wellifabio <wellifabio@hotmail.com>
2023-05-02 13:51:16 -0300

    aula04

--- 2des/04-indmo/aula04/clientes/source/screens/clientesScreen.js
+++ 2des/04-indmo/aula04/clientes/source/screens/clientesScreen.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import clientes from '../mocks/clientesMock'
+import { View, FlatList, StyleSheet } from 'react-native';
+import Clientes from '../mocks/clientesMock'
+import ItemLista from '../components/ItemLista'
 
-export default function ClientesScreen ({ navigation }){
-    return(
+export default function ClientesScreen({ navigation }) {
+    return (
         <View style={styles.container}>
-            <Text style={styles.text}>Lista de Clientes</Text>
-        </View>
-    );
-}
+            <FlatList
+                data={Clientes}
+                style={styles.list}
+                renderItem={({ item }) => <ItemLista props={item} />}
+            />
+        </View >);
+};
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
+    container: {
+        flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     list: {
         width: '100%',
         paddingHorizontal: 20,
-    },
-    text:{
-        fontSize: 20,
     }
 });
