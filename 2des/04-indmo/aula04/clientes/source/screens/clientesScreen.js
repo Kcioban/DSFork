0705fba29bd7fb6ead2b594f2cf0014d6bf372c0
wellifabio <wellifabio@hotmail.com>
2023-05-03 08:49:38 -0300

    aula04

--- 2des/04-indmo/aula04/clientes/source/screens/clientesScreen.js
+++ 2des/04-indmo/aula04/clientes/source/screens/clientesScreen.js
@@ -10,9 +10,11 @@ export default function ClientesScreen({ navigation }) {
                 style={styles.list}
                 renderItem={({ item }) => <View style={styles.item}>
                     <Image style={styles.img} source={item.avatar} />
-                    <Text>CPF: {item.cpf}</Text>
-                    <Text>Nome: {item.nome} {item.sobrenome}</Text>
-                    <Text>E-mail: {item.email}</Text>
+                    <View>
+                        <Text style={styles.text}>CPF: {item.cpf}</Text>
+                        <Text style={styles.text}>Nome: {item.nome} {item.sobrenome}</Text>
+                        <Text style={styles.text}>E-mail: {item.email}</Text>
+                    </View>
                 </View>}
             />
         </View >);
@@ -32,10 +34,16 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'row',
         margin: 10,
+        borderBottomColor: '#999',
+        borderBottomWidth: 1,
     },
     img: {
         width: 100,
         height: 100,
         margin: 5,
-    }
+    },
+    text: {
+        fontSize: 16,
+        margin: 8,
+    },
 });
