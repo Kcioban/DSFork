97a54c27f3fd9f07cf55a1c00fb28b46a279105e
wellifabio <wellifabio@hotmail.com>
2023-05-03 08:58:20 -0300

    aula04

--- 2des/04-indmo/aula04/clientes/App.js
+++ 2des/04-indmo/aula04/clientes/App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ClientesScreeen from './source/screens/ClientesScreen';
+import DetalhesScreen from './source/screens/DetalhesScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,10 @@ export default function App() {
           name="ClientesScreeen"
           component={ClientesScreeen}
           options={{ title: 'Lista de Clientes' }} />
+        <Stack.Screen
+          name="Detalhes"
+          component={DetalhesScreen}
+          options={{ title: 'Detalhes do Cliente' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
