9554d65899a1b7b6c86561d1c32d4782bba0c4a6
wellifabio <wellifabio@hotmail.com>
2023-05-09 10:31:30 -0300

    aula11

--- 2des/03-bcd/aula11/visoes.sql
+++ 2des/03-bcd/aula11/visoes.sql
@@ -24,3 +24,19 @@ SELECT c.nome FROM pedidos p INNER JOIN clientes c
 ON c.cliente_id = p.cliente_id
 WHERE p.pedido_id = 3;
 -- 6. Quais os nomes das pizzas do pedido nº 3
+SELECT * FROM itens_pedido WHERE pedido_id = 3;
+SELECT pizza_id FROM itens_pedido WHERE pedido_id = 3;
+SELECT * FROM pizzas WHERE pizza_id IN (7,5,8,9,3,10,16);
+-- Com SubConsulta
+SELECT * FROM pizzas WHERE pizza_id
+    IN (SELECT pizza_id FROM itens_pedido WHERE pedido_id = 3);
+SELECT nome FROM pizzas WHERE pizza_id
+    IN (SELECT pizza_id FROM itens_pedido WHERE pedido_id = 3);
+-- Com INNER JOIN
+SELECT p.nome FROM itens_pedido i INNER JOIN pizzas p
+ON i.pizza_id = p.pizza_id
+WHERE i.pedido_id = 3;
+-- Com INNER JOIN e mostrando todos o ítens do pedido
+SELECT i.*, p.nome FROM itens_pedido i INNER JOIN pizzas p
+ON i.pizza_id = p.pizza_id
+WHERE i.pedido_id = 3;
