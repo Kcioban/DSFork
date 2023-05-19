36382b7b1e4383a3a2bfe1120fe09da9d3332beb
ReenyeLima <reenyelima@hotmail.com>
2023-05-19 08:19:40 -0300

    aula12

--- 2des/02-pwfe/aula12/modal_cadastro_local/script.js
+++ 2des/02-pwfe/aula12/modal_cadastro_local/script.js
@@ -3,11 +3,11 @@ const detalhes = document.querySelector("#detalhes");
 const tcorpo = document.querySelector("#tcorpo");
 
 const dados = [
-    { "id": 1, "nome": "Leão" },
-    { "id": 2, "nome": "Elefante" },
-    { "id": 3, "nome": "Macaco" },
-    { "id": 4, "nome": "Cachorro" },
-    { "id": 5, "nome": "Gato" },
+    { "id": 10, "nome": "Celular" },
+    { "id": 20, "nome": "TV 50\"" },
+    { "id": 30, "nome": "Computador" },
+    { "id": 40, "nome": "Notebook" },
+    { "id": 50, "nome": "Caixa de som JBL" },
 ]
 
 btFechar.addEventListener('click', () => {
@@ -30,15 +30,15 @@ function preecherTabela() {
     })
 }
 
-function abrirModal(i){
+function abrirModal(i) {
     detalhes.classList.toggle("oculto");
     document.querySelector("#id").value = dados[i].id;
     document.querySelector("#nome").value = dados[i].nome;
 }
 
-function cadastarLocal(){
+function cadastarLocal() {
     const item = {
-        id : document.querySelector("#id").value,
+        id: document.querySelector("#id").value,
         nome: document.querySelector("#nome").value
     }
 
@@ -47,8 +47,8 @@ function cadastarLocal(){
     //Acrescentar o novo item na lista
     produtos.push(item)
     //Salvar a lista no armazenamento local
-    window.localStorage.setItem("produtos",JSON.stringify(produtos))
-    
+    window.localStorage.setItem("produtos", JSON.stringify(produtos))
+
     //Recarregar a página
     window.location.reload()
 }
