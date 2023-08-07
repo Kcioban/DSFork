eff5725189f6c7b74c3111449fceb7adca88e735
MatheusPignata <wellifabio@hotmail.com>
2023-08-07 08:58:18 -0300

    aula05

--- 1des/01-fpoo/aula05/README.md
+++ 1des/01-fpoo/aula05/README.md
@@ -22,4 +22,28 @@ int main(){
 - Desta forma faça com que o programa pergunte se o usuário é homem ou mulher e classifique de forma diferente
 - exp2.c
 ```c
+#include <stdio.h>
+int main(){
+	int i, sexo;
+	printf("Informe sua idade: ");
+	scanf("%d",&i);
+	printf("Informe como você de identifica 1.mulher, 2.homem:");
+	scanf("%d",&sexo);
+	if(sexo == 1){
+		if(i<10) printf("Criança");
+		else if(i<15) printf("Adolescente");
+		else if(i<20) printf("Jovem");
+		else if(i<35) printf("Adulto");
+		else if(i<55) printf("Meia idade");
+		else printf("Idoso");
+	} else {
+		if(i<20) printf("Criança");
+		else if(i<35) printf("Adolescente");
+		else if(i<40) printf("Jovem");
+		else if(i<50) printf("Adulto");
+		else if(i<55) printf("Meia idade");
+		else printf("Idoso");
+	}
+	return 0;
+}
 ```
--- 1des/01-fpoo/aula05/exemplos/exp1.c
+++ 1des/01-fpoo/aula05/exemplos/exp1.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 int main(){
-	int i;
+	int i, sexo;
 	printf("Informe sua idade: ");
 	scanf("%d",&i);
-	if(i<10) printf("Criança");
-	else if(i<15) printf("Adolescente");
-	else if(i<20) printf("Jovem");
-	else if(i<35) printf("Adulto");
-	else if(i<55) printf("Meia idade");
-	else printf("Idoso");
+	printf("Informe como você de identifica 1.mulher, 2.homem:");
+	scanf("%d",&sexo);
+	if(sexo == 1){
+		if(i<10) printf("Criança");
+		else if(i<15) printf("Adolescente");
+		else if(i<20) printf("Jovem");
+		else if(i<35) printf("Adulto");
+		else if(i<55) printf("Meia idade");
+		else printf("Idoso");
+	} else {
+		if(i<20) printf("Criança");
+		else if(i<35) printf("Adolescente");
+		else if(i<40) printf("Jovem");
+		else if(i<50) printf("Adulto");
+		else if(i<55) printf("Meia idade");
+		else printf("Idoso");
+	}
 	return 0;
 }
